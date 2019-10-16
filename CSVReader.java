9ceda0bd1b72a9990b6194d5565433d32c6cca1f
Justin Lane <jclane@users.noreply.github.com>
2019-10-16 12:34:14 -0400

    Added support for ```Path``` objects

--- CSVReader.java
+++ CSVReader.java
@@ -1,46 +1,33 @@
 package me.justinlane.simplecsv;
 
-class CSVReader {
-  
-  private String delimit = ",";
-  private ArrayList<ArrayList<String>> lines = new ArrayList<ArrayList<String>>();
-  
-  public Reader(String filePath, String delimit) {
-    setDelimit(delimit);
-    try {
-      this.lines = readFile(filePath);
-    } catch (FileNotFoundException e) {
-      System.out.println("ERR: File not found.");
-    }
-  }
-  
-  public Reader(String filePath) {
-    try {
-      this.lines = readFile(filePath);
-    } catch (FileNotFoundException e) {
-      System.out.println("ERR: File not found.");
-    }
-  }
-  
-  public String getDelimit() {
-    return this.delimit;
-  }
-  
-  private void setDelimit(String delimit) {
-    this.delimit = delimit;
-  }
-  
-  public ArrayList<ArrayList<String>> getLines() {
-    return this.lines;    
-  }
+import java.io.File;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+
+import java.nio.file.Path;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * A simple CSV parser.
+ *
+ * @author Justin Lane
+ * @version 4.0
+ * @since 4.0
+ */
+public class CSVReader {
   
   /**
    * This will ignore delimiters found within quotes and 
    * remove the quote characters.
    * 
-   * Most code from: https://stackoverflow.com/a/2120714
+   * @param str the string to check for double quotes
+   * @param delimiter the string to use as a delimiter   
+   * @see <a href=https://stackoverflow.com/a/2120714 />
    */  
-  private List<String> handleQuotes(String str) {
+  private static List<String> handleQuotes(String str, String delimiter) {
     List<String> result = new ArrayList<String>();
     int start = 0;
     boolean inQuotes = false;
@@ -48,34 +35,76 @@ class CSVReader {
         if (str.charAt(current) == '\"') inQuotes = !inQuotes; // toggle state
         boolean atLastChar = (current == str.length() - 1);
         if(atLastChar) result.add(str.substring(start));
-        else if (str.charAt(current) == this.delimit.charAt(0) && !inQuotes) {
-            result.add(str.substring(start, current).replace("\"", ""));
+        else if (str.charAt(current) == delimiter.charAt(0) && !inQuotes) {
+            result.add(str.substring(start, current).replace(delimiter, ""));
             start = current + 1;
         }
     }
     return result;
   }  
-   
-  private ArrayList<ArrayList<String>> readFile(String filePath) throws FileNotFoundException {
-    Scanner scanner = new Scanner(new File(filePath));
+  
+  /** 
+   * Reads filePath using delimit as the delimiter.
+   * 
+   * @param filePath the path to the file to be read as type Path
+   * @param delimit the string to use a as delimiter
+   * @return a list of lists with string values from the file
+   */
+  public static List<List<String>> readLines(Path filePath, String delimit) throws IOException {
+    Scanner scanner = new Scanner(filePath);
+    List<List<String>> lines = new ArrayList<List<String>>();
     while (scanner.hasNextLine()) {
-      ArrayList<String> columnData = new ArrayList<String>();
-      for (String data : handleQuotes(scanner.nextLine())) {
+      List<String> columnData = new ArrayList<String>();
+      for (String data : handleQuotes(scanner.nextLine(), delimit)) {
         columnData.add(data); 
       }    
-      this.lines.add(columnData);        
+      lines.add(columnData);        
     }
     scanner.close();
     
-    return this.lines;
+    return lines;
+  }  
+  
+  /**
+   * Reads filePath using commas as the delimiter.
+   * 
+   * @param filePath the path to the file to be read as type Path
+   * @return a list of lists with string values from the file
+   */
+  public static List<List<String>> readLines(Path filePath) throws IOException {
+    return readLines(filePath, ",");
   }
   
-  public void printLines() {
-    if (this.lines.size() > 0) {
-      for (ArrayList<String> line : this.lines) {
-        System.out.println(String.join(delimit, line));
-      }
+  /** 
+   * Reads filePath using delimit as the delimiter.
+   * 
+   * @param filePath the string representing the path to a file
+   * @param delimit the string to use a as delimiter
+   * @return a list of lists with string values from the file
+   */
+  public static List<List<String>> readLines(String filePath, String delimit) throws FileNotFoundException {
+    Scanner scanner = new Scanner(new File(filePath));
+    List<List<String>> lines = new ArrayList<List<String>>();
+    while (scanner.hasNextLine()) {
+      List<String> columnData = new ArrayList<String>();
+      for (String data : handleQuotes(scanner.nextLine(), delimit)) {
+        columnData.add(data); 
+      }    
+      lines.add(columnData);        
     }
-  }
+    scanner.close();
     
-} 
+    return lines;
+  }  
+  
+  /**
+   * Reads filePath using commas as the delimiter.
+   * 
+   * @param filePath path to file
+   * @return a list of lists with string values from the file
+   */
+  public static List<List<String>> readLines(String filePath) throws FileNotFoundException {
+    return readLines(filePath, ",");
+  }
+
+}
--- CSVWriter.java
+++ CSVWriter.java
@@ -1,17 +1,78 @@
 package me.justinlane.simplecsv;
 
-class CSVWriter {
+import java.io.FileWriter;
+import java.io.IOException;
+
+import java.nio.file.Path;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A simple CSV writer.
+ *
+ * @author Justin Lane
+ * @version 4.0
+ * @since 4.0
+ */
+public class CSVWriter {
   
-  private String delimit = ",";
-  
-  public void writeLines(ArrayList<ArrayList<String>> data, String filePath) throws IOException {
+  /**
+   * Creates *.CSV file with data as the content using delimit as a delimiter.
+   * 
+   * @param filePath a Path indicating the path and file name
+   * @param data a list of lists with string data to save
+   * @param delimit a string use as a delimiter
+   */ 
+  public static void writeLines(Path filePath, List<List<String>> data, String delimit) 
+       throws IOException {
+    FileWriter fw = new FileWriter(filePath.toFile());
+    for (List<String> row : data) {
+      fw.append(String.join(delimit, row));
+      fw.append("\n");
+    }
+    fw.flush();
+    fw.close();
+  }  
+   
+  /**
+   * Creates *.CSV file with data as the content using comma as a delimiter.
+   * 
+   * @param filePath a string indicating the path and file name
+   * @param data a list of lists with string data to save
+   */
+  public static void writeLines(Path filePath, List<List<String>> data) 
+      throws IOException {
+    writeLines(filePath, data, ",");
+  }   
+   
+   
+  /**
+   * Creates *.CSV file with data as the content using delimit as a delimiter.
+   * 
+   * @param filePath a string indicating the path and file name
+   * @param data a list of lists with string data to save
+   * @param delimit a string use as a delimiter
+   */ 
+  public static void writeLines(String filePath, List<List<String>> data, String delimit) 
+       throws IOException {
     FileWriter fw = new FileWriter(filePath);
-    for (ArrayList<String> row : data) {
-      fw.append(String.join(this.delimit, row));
+    for (List<String> row : data) {
+      fw.append(String.join(delimit, row));
       fw.append("\n");
     }
     fw.flush();
     fw.close();
   }
   
+  /**
+   * Creates *.CSV file with data as the content using comma as a delimiter.
+   * 
+   * @param filePath a string indicating the path and file name
+   * @param data a list of lists with string data to save
+   */
+  public static void writeLines(String filePath, List<List<String>> data) 
+      throws IOException {
+    writeLines(filePath, data, ",");
+  }
 }
